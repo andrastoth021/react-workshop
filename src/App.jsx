e3af180c6a78296e900b769c52d02ba7e2679920
Andras Toth <125831134+andrastoth021@users.noreply.github.com>
2023-06-13 11:10:48 +0200

    Implement Dice component (Task 1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // use nextId() to generate a new unique ID when needed
 import nextId from "./generateId";
+import Dice from "./components/Dice";
 
 function App() {
   return (
-    <>
+    <div>
       <p>Hello from the App!</p>
-    </>
+      <Dice number={3}/>
+    </div>
   );
 }
 
---
+++ src/components/Dice.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Dice(props) {
+  const number = props.number;
+  return (
+    <h2>{number}</h2>
+  )
+}
+
+export default Dice
